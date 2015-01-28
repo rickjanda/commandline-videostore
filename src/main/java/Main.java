0d63b220a88f2471b2523da038df53d9e921c357
rja <rick.janda@zuehlke.com>
2015-01-28 19:21:08 +0100

    initial import: command line movie store with pin-down test and Maven POM

---
+++ src/main/java/Main.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private final InputStream in;
+    private final PrintStream out;
+
+    public static void main(String[] args) throws IOException {
+        new Main(System.in, System.out);
+    }
+
+    public Main(InputStream in, PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    void run() throws IOException {
+        // read movies from file
+        final InputStream movieStream = Main.class.getResourceAsStream("/movies.cvs");
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(movieStream));
+        final List<String[]> movies = new ArrayList<>();
+        int movieNumber = 0;
+        while (bufferedReader.ready()) {
+            final String line = bufferedReader.readLine();
+            final String[] movie = line.split(";");
+            movies.add(movie);
+            out.print(movieNumber + ": " + movie[0] + "\n");
+            movieNumber++;
+        }
+
+        final BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(in));
+        out.print("Enter customer name: ");
+        String customerName = inputStreamReader.readLine();
+
+        out.print("Choose movie by number followed by rental days, just ENTER for bill:\n");
+
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + customerName + "\n";
+        while (true) {
+            String input = inputStreamReader.readLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            final String[] rental = input.split(" ");
+            final String[] movie = movies.get(Integer.parseInt(rental[0]));
+            double thisAmount = 0;
+
+            int daysRented = Integer.parseInt(rental[1]);
+            //determine amounts for rental
+            switch (movie[1]) {
+                case "REGULAR":
+                    thisAmount += 2;
+                    if (daysRented > 2)
+                        thisAmount += (daysRented - 2) * 1.5;
+                    break;
+                case "NEW_RELEASE":
+                    thisAmount += daysRented * 3;
+                    break;
+                case "CHILDRENS":
+                    thisAmount += 1.5;
+                    if (daysRented > 3)
+                        thisAmount += (daysRented - 3) * 1.5;
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if (movie[1].equals("NEW_RELEASE") && daysRented > 1) {
+                frequentRenterPoints++;
+            }
+            // show figures for this rental
+            result += "\t" + movie[0] + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines
+        result += "You owed " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+
+        out.print(result);
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,59 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+/**
+ * Simple pin-down test. Do not use this as an example for a good test ;-)
+ */
+public class MainTest {
+
+    @Test
+    public void run_fullCoverage() throws Exception {
+        final String consoleInput =
+                "John Doe\n" +
+                "0 1\n" +
+                "2 3\n" +
+                "6 1\n" +
+                "9 1\n" +
+                "10 2\n" +
+                "13 5\n" +
+                "\n";
+        final ByteArrayOutputStream consoleOutputStream = new ByteArrayOutputStream();
+
+        new Main(
+                new ByteArrayInputStream(consoleInput.getBytes(StandardCharsets.UTF_8)),
+                new PrintStream(consoleOutputStream)
+        ).run();
+
+        assertEquals("0: The Shawshank Redemption (1994)\n" +
+                        "1: The Godfather (1972)\n" +
+                        "2: The Godfather: Part II (1974)\n" +
+                        "3: The Dark Knight (2008)\n" +
+                        "4: Pulp Fiction (1994)\n" +
+                        "5: The Good, the Bad and the Ugly (1966)\n" +
+                        "6: The Lord of the Rings: The Return of the King (2003)\n" +
+                        "7: Fight Club (1999)\n" +
+                        "8: The Lord of the Rings: The Fellowship of the Ring (2001)\n" +
+                        "9: Interstellar (2014)\n" +
+                        "10: Whiplash (2014)\n" +
+                        "11: Birdman (2014)\n" +
+                        "12: Up (2009)\n" +
+                        "13: WALL·E (2008)\n" +
+                        "Enter customer name: Choose movie by number followed by rental days, just ENTER for bill:\n" +
+                        "Rental Record for John Doe\n" +
+                        "\tThe Shawshank Redemption (1994)\t2.0\n" +
+                        "\tThe Godfather: Part II (1974)\t3.5\n" +
+                        "\tThe Lord of the Rings: The Return of the King (2003)\t1.5\n" +
+                        "\tInterstellar (2014)\t3.0\n" +
+                        "\tWhiplash (2014)\t6.0\n" +
+                        "\tWALL·E (2008)\t4.5\n" +
+                        "You owed 20.5\n" +
+                        "You earned 7 frequent renter points\n",
+                consoleOutputStream.toString("UTF-8"));
+    }
+}
